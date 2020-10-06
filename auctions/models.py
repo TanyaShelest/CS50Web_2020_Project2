24cd1ee5551c7df9e453624c1ca93c48d39e5798
Tetiana Shelest <tashelest@gmail.com>
2020-10-06 22:10:02 +0300

    Create models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,25 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_price = models.IntegerField()
+    category = models.ManyToManyField(Category) 
+
+
+class Bid(models.Model):
+    value = models.IntegerField()
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('Publication date')
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    comment_text = models.TextField()
