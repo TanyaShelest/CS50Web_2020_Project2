b9b49a0640e2ee7585f038497fa0e4266b717aa8
Tetiana Shelest <tashelest@gmail.com>
2020-10-18 21:03:23 +0300

    Implement an admin interface to view, add, and edit items

--- auctions/admin.py
+++ auctions/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 
 from .models import Category, Bid, Listing, Comment
 
-admin.site.register(Category)
-admin.site.register(Bid)
-admin.site.register(Comment)
-admin.site.register(Listing)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+admin.site.register(Category, CategoryAdmin)
+
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ['listing', 'author', 'value']
+admin.site.register(Bid, BidAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['listing', 'pub_date', 'author', 'text']
+admin.site.register(Comment, CommentAdmin)
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ['title', 'current_price', 'pub_date', 'author', 'active']
+admin.site.register(Listing, ListingAdmin)
